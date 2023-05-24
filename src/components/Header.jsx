6aa028612683a213b347a77fed1880402329b8c9
veronicapava <veronicapava@gmail.com>
2023-05-24 10:55:03 -0500

    Crea pagina de inicio y header

---
+++ src/components/Header.jsx
@@ -0,0 +1,52 @@
+import React from "react"
+
+const Header = () => {
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <a className="navbar-brand" href="/">
+            Home
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="/createUser">
+                  Create user
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  Find User
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  Update User
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  User List
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Home = () => {
+  return (
+    <div className="container">
+      <p className="lead">This is a page to user management.</p>
+      <p>Feel free to explore the site and discover more.</p>
+    </div>
+  )
+}
+
+export default Home
